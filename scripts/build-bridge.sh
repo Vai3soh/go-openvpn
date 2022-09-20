9dd71c8382ed0883fb100ca458bb520012634063
Vai3soh <work.rwx.seven@gmail.com>
2022-09-21 09:54:00 +1000

    Uncomment build windows system, build library for win

--- scripts/build-bridge.sh
+++ scripts/build-bridge.sh
@@ -22,7 +22,7 @@ echo "O3 is: $O3"
 
 /go-src-root/scripts/x-compile-linux.sh
 #/go-src-root/scripts/x-compile-mac.sh
-#/go-src-root/scripts/x-compile-windows.sh
+/go-src-root/scripts/x-compile-windows.sh
 #/go-src-root/scripts/x-compile-ios.sh
 #/go-src-root/scripts/x-compile-android-arm64.sh
 #/go-src-root/scripts/x-compile-android-armeabi-v7a.sh
